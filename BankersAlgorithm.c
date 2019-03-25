1c318918ae6cab3feb460b145a933e41c606c1d6
Abhishek verma <48916385+avas27@users.noreply.github.com>
2019-03-25 21:37:46 +0530

    Add files via upload

---
+++ BankersAlgorithm.c
@@ -0,0 +1,48 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<pthread.h>
+#include<sys/types.h>
+#include<stdlib.h>
+void avas1();
+int main()
+{
+system("clear");
+printf("\n\t\t\tBanker's_Algorithm\n");
+printf("\t\t\tCSE-316 Project\n");
+printf("\t\t_________________________________________\n");
+printf("\t\t---------------------------------------\n");
+printf("\t\tCREATED BY ::\n");
+printf("\t\t~~~~~~~~~~~~~~~~~~~~\n");
+printf("\t\tAbhishek Verma\n");
+printf("\t\tRegistration No: 11702561\n");
+printf("\t\tRoll No: 63\n");
+printf("\t\tSection: K17AP\n");
+printf("\t\tASSIGNED BY ::\n");
+printf("\t\t~~~~~~~~~~~~~~~~~~~~\n");
+printf("\t\tMr. Balgit Singh Saini\n");
+printf("\n\t\t-----------------------------------------\n");
+printf("\t\tLOVELY PROFESSIONAL UNIVERSITY\tavas 2019\n");
+printf("\t\t_________________________________________\n");
+printf("\t\t-----------------------------------------\n");
+printf("\t\tPRESS (1) to CONTINUE\n");
+
+//avas1();
+}
+void avas1()
+{
+system("clear");
+printf("\n\t\tEnter number of PROCESSES\n");
+printf("\t\t-----------------------------------------\n");
+printf("\t\t\tfor each PROCESS (P1,P2,...)\n");
+printf("\t\t-----------------------------------------\n");
+printf("\t\tEnter MAXIMUM resources required\n");
+printf("\t\tEnter AVAILABLE resources\n");
+printf("\t\tEnter ALLOCATED no of resources\n");
+printf("\t\t_________________________________________\n");
+printf("\t\t-----------------------------------------\n");
+printf("\t\tPRESS (1) to check SAFE-STATE\n");
+printf("\t\tPRESS (2) to grant RESOURCES\n");
+printf("\t\tPRESS (3) to request RESOURCES\n");
+}
+
+

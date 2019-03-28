bcadf1f5a26f970f37a8a5ec2779d194bf8fc42a
Abhishek verma <48916385+avas27@users.noreply.github.com>
2019-03-28 22:06:27 +0530

    Add files via upload

--- BankersAlgorithm.c
+++ BankersAlgorithm.c
@@ -4,30 +4,34 @@
 #include<sys/types.h>
 #include<stdlib.h>
 int n,n2,np,na,r=0,counter=0,count=0;
-int avas[10][10][10],avasAvail[10];
+int avas[10][10][10],avasAvail[10],avasState[10],avasFormat[1][10][10];
 void avas1();
 void avas2();
 void avas3();
+void avas4();
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
 int main()
 {
 system("clear");
 printf("\n\t\t\tBanker's_Algorithm\n");
 printf("\t\t\tCSE-316 Project\n");
-printf("\t\t_________________________________________\n");
-printf("\t\t---------------------------------------\n");
+printf("\t\t_____________________________________________\n");
+printf("\t\t---------------------------------------------\n");
 printf("\t\tCREATED BY ::\n");
 printf("\t\t~~~~~~~~~~~~~~~~~~~~\n");
 printf("\t\tAbhishek Verma\n");
 printf("\t\tRegistration No: 11702561\n");
 printf("\t\tRoll No: 63\n");
 printf("\t\tSection: K17AP\n");
-printf("\t\tASSIGNED BY ::\n");
+printf("\n\t\tASSIGNED BY ::\n");
 printf("\t\t~~~~~~~~~~~~~~~~~~~~\n");
-printf("\t\tMr. Balgit Singh Saini\n");
-printf("\n\t\t-----------------------------------------\n");
-printf("\t\tLOVELY PROFESSIONAL UNIVERSITY\t||avas 2019\n");
-printf("\t\t_________________________________________\n");
-printf("\t\t-----------------------------------------\n");
+printf("\t\tMr. Baljit Singh Saini\n");
+printf("\n\t\t--------------------------------------------\n");
+printf("\t\tLOVELY PROFESSIONAL UNIVERSITY || avas 2019\n");
+printf("\t\t_____________________________________________\n");
+printf("\t\t---------------------------------------------\n");
 printf("\t\tPRESS (1) to CONTINUE\n");
 int n;
 printf("\t\t");
@@ -41,7 +45,9 @@ default:
 printf("\n\t\tplease PRESS valid option\n");
 }
 }
-/////////////////////////////////////////////////////////////avas 2019///////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
 void avas1()
 {
 system("clear");
@@ -49,7 +55,7 @@ printf("\n\t\t\tBanker's_Algorithm\n");
 printf("\t\t\tCSE-316 Project\n");
 printf("\t\t_________________________________________\n");
 printf("\t\t---------------------------------------\n");
-printf("\n\t\tEnter number of RESOURSES types available\n");
+printf("\n\t\tEnter number of RESOURSES types\n");
 printf("\t\t");
 scanf("%d",&np);
 printf("\n\t\tEnter number of PROCESSES\n");
@@ -90,26 +96,40 @@ r++;
 count=0;
 printf("\n");
 }
+
 printf("\t\tEnter AVAILABLE no of resources\n");
 for(int i=0;i<np;i++)
 {
 printf("\t\tR%d : ",(++count));
 scanf("%d",&na);
-avasAvail[i]=n2;
+avasAvail[i]=na;
+//avasFormat[1][i]=na;
 }
 printf("\t\t_________________________________________\n");
 printf("\t\t-----------------------------------------\n");
 printf("\t\tPRESS (1) to check SAFE-STATE\n");
 printf("\t\tPRESS (2) to grant RESOURCES\n");
 printf("\t\tPRESS (3) to request RESOURCES\n");
+printf("\t\t");
+int m;
+scanf("%d",&m);
+switch(m)
+{
+case 1:
+avas2();
+break;
+default:
+printf("Please enter VALID option\n");
+}
 //for(int i=0;i<2;i++)
 //{
-//for(int j=0;j<2;j++)
-//printf("%d\n",avas[0][j][i]);
+//for(int j=0;j<np;j++)
+//printf("%d\n",avasAvail[j]);
 //}
-avas2();
 }
-
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
 void avas2()
 {
 r=0;
@@ -129,22 +149,102 @@ for(int i=0;i<2;i++)
 printf("%d\n",avas[2][1][i]);
 */
 }
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
 
 void avas3()
 {
+int x=0,kachra=0;
+for(int z=0;z<n;z++)
+{
 r=0;
-int x=0;
 for(int j=0;j<n;j++)
 {
 for(int i=0;i<np;i++)
 {
+//printf("%d\n",avas[2][r][i]);
+//printf("%d\n",avasAvail[i]);
 if(avas[2][r][i]<=avasAvail[i])
 {x=1;}
 else
 {x=0;}
 }
+//printf("ok %d\n",x);
+if(x==1)
+{
+avasState[kachra]=(r+1);
+kachra++;
+for(int k=0;k<np;k++)
+{
+avasAvail[k]+=avas[1][r][k];
+avasFormat[0][r][k]=avas[2][r][k];
+avas[2][r][k]=100;
+}
+//printf("%d\n",avasAvail[0]);
+//printf("%d\n",avasAvail[1]);
+}
 r++;
 }
-if(x==1)
-printf("wow\n");
+
+}
+//int rr=sizeof(avasState)/sizeof(avasState[0]);
+//printf("%d",rr);
+/*printf("<");
+for(int i=0;i<n;i++)
+{printf("P%d ",avasState[i]);}
+printf(">\n");
+*/
+avas4();
+}
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////avas 2019//////////////////////////////////////////////////////////////////
+void avas4()
+{
+r=0;
+system("clear");
+printf("\n\t\t\tBanker's_Algorithm\n");
+printf("\t\t\tCSE-316 Project\n");
+printf("\t\t_________________________________________\n");
+printf("\t\t---------------------------------------\n");
+printf("\t\t****************************************\n");
+printf("\t\tPROCESS\t MAX \t ALLOCATED \tNEED\n");
+for(int i=0;i<n;i++)
+{
+printf("\t\t  P%d",(i+1));
+printf("\t");
+for(int j=0;j<np;j++)
+{
+printf(" %d",avas[0][r][j]);
+}
+printf("\t");
+for(int j=0;j<np;j++)
+{
+printf(" %d",avas[1][r][j]);
+}
+printf("\t\t");
+for(int j=0;j<np;j++)
+{
+printf(" %d",avasFormat[0][r][j]);
+}/*
+printf("\t");
+for(int j=0;j<np;j++)
+{
+printf(" %d",avasFormat[j][1]);
+}*/
+printf("\n");
+r++;
+}
+printf("\n\t\t****************************************\n");
+printf("\t\tSAFE-STATE is : < ");
+for(int i=0;i<n;i++)
+{
+printf("P%d ",avasState[i]);
+}
+printf(">\n");
+printf("\t\t_________________________________________\n");
+printf("\t\t-----------------------------------------\n");
+printf("\t\tPRESS (1) to go on MAIN-MENU\n");
+printf("\t\tPRESS (2) to EXIT\n");
 }
